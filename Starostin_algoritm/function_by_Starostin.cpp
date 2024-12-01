16a8b8c83cc8188aa06598e9fb278e36b6da7f4f
MS-Semka <arsenijstarostin@gmail.com>
2024-12-01 19:50:57 +0300

    3

--- Starostin_algoritm/function_by_Starostin.cpp
+++ Starostin_algoritm/function_by_Starostin.cpp
@@ -1,3 +1,4 @@
+<<<<<<< Updated upstream
 //
 //  function_by_Vodorezova.cpp
 //  Homework_C-
@@ -5,3 +6,72 @@
 //  Created by Maxim Gvozdev on 21.10.2024.
 //
 
+=======
+#include <iostream>
+#include "function_by_Starostin.h"
+#include <vector>
+#include <string>
+
+using namespace std;
+
+void create_Zarr(const string& str, vector<int>& Z) {
+    int n = str.length();
+    int left = 0, right = 0;
+
+    Z.resize(n, 0);
+
+    for (int i = 1; i < n; ++i) {
+        if (i > right) {
+            left = right = i;
+            while (right < n && str[right] == str[right - left])
+                right++;
+            Z[i] = right - left;
+            right--;
+        }
+        else {
+            int k = i - left;
+            if (Z[k] < right - i + 1) {
+                Z[i] = Z[k];
+            }
+            else {
+                left = i;
+                while (right < n && str[right] == str[right - left])
+                    right++;
+                Z[i] = right - left;
+                right--;
+            }
+        }
+    }
+}
+
+bool z_algoritm(const string& text, const string& pattern) {
+    string concat = pattern + "$" + text;
+    int l = concat.length();
+    vector<int> Z;
+    bool u;
+
+    create_Zarr(concat, Z);
+
+    int count = 0;
+
+    for (int i = 0; i < l; ++i) {
+        if (Z[i] == pattern.length() && pattern.length() != 0) {
+            u = true;
+            break;
+            //count++;
+        }
+        else {
+            u = false;
+        }
+    }
+    return u;
+}
+
+// Driver program
+int main() {
+    string text = "aaabbbvvvggg";
+    string pattern = "bbb";
+    z_algoritm(text, pattern);
+    return 0;
+}
+>>>>>>> Stashed changes
--- Starostin_algoritm/function_by_Starostin.h
+++ Starostin_algoritm/function_by_Starostin.h
@@ -1,3 +1,4 @@
+<<<<<<< Updated upstream
 //
 //  function_by_Vodorezova.h
 //  Homework_C-
@@ -19,4 +20,16 @@
 //
 //  Created by Maxim Gvozdev on 21.10.2024.
 //
+=======
+#include <iostream>
+#include <vector>
+#include <string>
+>>>>>>> Stashed changes
 
+using namespace std;
+
+void create_Zarr(const string& str, vector<int>& Z);
+
+bool z_algoritm(const string& text, const string& pattern);
+
+int main();
