1d101c276055bc8c43ce157e9226d27a1706471a
Maxim <maxpersik@gmail.com>
2024-12-01 20:02:00 +0300

    Gevorkyan added

---
+++ Gevorkyan_algoritm/function_by_Gevorkyan.cpp
@@ -0,0 +1,7 @@
+//
+//  function_by_Ge.swift
+//  Homework_Cpp
+//
+//  Created by Maxim Gvozdev on 01.12.2024.
+//
+
---
+++ Gevorkyan_algoritm/function_by_Gevorkyan.h
@@ -0,0 +1,7 @@
+//
+//  function_by_Gevorkyan.h
+//  Homework_Cpp
+//
+//  Created by Maxim Gvozdev on 01.12.2024.
+//
+
